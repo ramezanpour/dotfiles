0ca3a9f52fb142064d7f49bda2a3cc479d9f8d59
Mohammad Mahdi Ramezanpour <i@ramezanpour.net>
2017-03-31 01:44:13 +0430

    Add redshift and dropbox to xinitrc

--- arch_config/packages.sh
+++ arch_config/packages.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-set e
 
 # Essential packages
-sudo pacman -Sy vlc qt4 htop tmux vim dialog wpa_supplicant iw i3 dmenu feh firefox git cmake pulseaudio pulseaudio-alsa alsa-utils openconnecti openssh screenfetch zsh --needed
+sudo pacman -S vlc qt4 htop tmux vim dialog wpa_supplicant iw i3 dmenu feh firefox git cmake pulseaudio pulseaudio-alsa alsa-utils openconnect openssh screenfetch zsh redshift ranger ntfs-3g unrar linux-headers linux --needed
+
+# Yaourt
+
+mkdir ~/Temp
+cd ~/Temp
+git clone https://aur.archlinux.org/package-query.git
+cd ~/Temp/package-query
+makepkg -si
+cd ~/Temp
+git clone https://aur.archlinux.org/yaourt.git
+cd ~/Temp/yaourt
+makepkg -si
+
+# Install dropbox
+yaourt -S dropbox turses
+
+
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
