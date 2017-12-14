b9a2e8923c4c8745e0bcd225c5e8ba9303fccc97
Mohammad Mahdi Ramezanpour <i@ramezanpour.net>
2017-12-14 19:01:07 +0330

    Update packages.sh
    Added some additional configuration to make it even easier.

--- arch_config/packages.sh
+++ arch_config/packages.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Create projects folder
+
+mkdir -p ~/projects
+
+# Updating repos
+sudo pacman -Suy
+
 # Essential packages
-sudo pacman -S vlc qt4 htop tmux vim dialog wpa_supplicant iw i3 dmenu feh firefox git cmake pulseaudio pulseaudio-alsa alsa-utils openconnect openssh screenfetch zsh redshift ranger ntfs-3g unrar linux-headers linux powerline lm_sensors libreoffice-still goldendict thunderbird transmission-gtk links sdcv youtube-dl aria2 units rxvt-unicode  --needed
+sudo pacman -S xorg xorg-xinit geary htop tmux vim dialog wpa_supplicant iw git cmake openconnect openssh screenfetch zsh redshift ranger ntfs-3g unzip unrar linux-headers linux powerline lm_sensors libreoffice-still goldendict transmission-gtk links sdcv youtube-dl aria2 units clang ttf-dejavu ttf-liberation noto-fonts python python-pip --needed
 
 # Yaourt
-
 mkdir ~/Temp
 cd ~/Temp
 git clone https://aur.archlinux.org/package-query.git
@@ -15,21 +21,25 @@ git clone https://aur.archlinux.org/yaourt.git
 cd ~/Temp/yaourt
 makepkg -si
 
-# Install dropbox
-yaourt -S dropbox turses
+# Install yaourt packages
+yaourt -S dropbox google-chrome postman-bin slack-desktop
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# VIM configuration
 
 # Vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Powerline fonts
-mkdir ~/Temp
-cd ~/Temp
-git clone https://github.com/powerline/fonts.git --depth=1
-cd ~/Temp/fonts
-./install.sh
-cd ~/
-rm -rf ~/Temp
+# VIMRC
+cp ~/projects/dotfiles/.vimrc ~/.vimrc
 
+vim +PluginInstall
 
-# Oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Build YCM
+
+~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-clang
+
+cp ~/projects/dotfiles/.zshrc ~/.zshrc
+cp ~/projects/dotfiles/tmux.conf ~/.tmux.conf
