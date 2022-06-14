4ae290c3460ee6365b1705a76b86bd2892cacc25
Mohammad M. Ramezanpour <i@ramezanpour.net>
2022-06-14 23:30:29 +0200

    Create new file having some aliases for note taking and journaling

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Install OMZ
-
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Vim Plugged
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 
