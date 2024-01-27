e7c9f7ff87b9921d498736750d51e646b88bdbc8
Mohammad M. Ramezanpour <i@ramezanpour.net>
2024-01-27 11:46:47 +0100

    Update brewfile and bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Copy tmux config
-cp ./tmux.conf ~/.tmux.conf
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # Install tmux theme
 mkdir -p ~/.tmux/themes
-git clone https://github.com/arcticicestudio/nord-tmux ~/.tmux/themes/
+git clone https://github.com/arcticicestudio/nord-tmux ~/.tmux/themes/nord-tmux
 
 # This is a requirement for OMZ colorize plugin
 pip3 install Pygments
 
 
 # Download the Nord theme for iterm to the Downloads folder so that it can be applied
-curl -O https://github.com/nordtheme/iterm2/blob/develop/src/xml/Nord.itermcolors
+curl --output-dir ~/Downloads -O https://github.com/nordtheme/iterm2/blob/develop/src/xml/Nord.itermcolors
 
 # Download 24-bit test script to the home folder
 curl -O https://gist.githubusercontent.com/lifepillar/09a44b8cf0f9397465614e622979107f/raw/24-bit-color.sh
@@ -20,4 +21,9 @@ curl -O https://gist.githubusercontent.com/lifepillar/09a44b8cf0f9397465614e6229
 # Some nvim plugins require yarn to be installed
 npm install -g yarn
 
+# Install vim Plugged
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# Install OMZ
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
