0bc22c7358090212142c11457870379a98018ae6
Mohammad M. Ramezanpour <i@ramezanpour.net>
2024-03-11 10:08:27 +0100

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,9 +14,10 @@ pip3 install Pygments
 # Download the Nord theme for iterm to the Downloads folder so that it can be applied
 curl --output-dir ~/Downloads -O https://github.com/nordtheme/iterm2/blob/develop/src/xml/Nord.itermcolors
 
-# Download 24-bit test script to the home folder
-curl -O https://gist.githubusercontent.com/lifepillar/09a44b8cf0f9397465614e622979107f/raw/24-bit-color.sh
+open ~/Downloads/Nord.itermcolors
 
+# Download 24-bit test script to the home folder
+curl --output-dir ~/ -O https://gist.githubusercontent.com/lifepillar/09a44b8cf0f9397465614e622979107f/raw/24-bit-color.sh
 
 # Some nvim plugins require yarn to be installed
 npm install -g yarn
@@ -27,3 +28,15 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 # Install OMZ
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+mkdir -p ~/.config/nvim
+cp -n ./nvim/* ~/.config/nvim/
+
+mkdir -p ~/.config/bat
+cp -n ./bat/config ~/.config/bat/config
+
+mkdir -p ~/.config/tmux/
+cp -n ./tmux/tmux.conf ~/.config/tmux/tmux.conf
+
+mkdir -p ~/.oh-my-zsh/themes
+cp -n ./oh-my-zsh/themes/agnoster-moh.zsh-theme ~/.oh-my-zsh/themes/agnoster-moh.zsh-theme
--- zshrc
+++ zshrc
@@ -1,6 +1,6 @@
 export ZSH="$HOME/.oh-my-zsh"
 
-ZSH_THEME="robbyrussell"
+ZSH_THEME="agnoster-moh"
 
 plugins=(
     git
@@ -12,19 +12,23 @@ plugins=(
     colorize
     direnv
     tmux
+    aws
 )
 
 autoload -U compinit && compinit
 
 source $ZSH/oh-my-zsh.sh
 
-alias tmux="tmux -2"
 
-alias edviconf="nvim ~/.config/nvim/init.vim"
-alias edtmuxconf="nvim ~/.tmux.conf"
-alias edzshconf="nvim ~/.zshrc"
+alias edvconf="nvim ~/.config/nvim/init.vim"
+alias edtconf="nvim ~/.config/tmux/tmux.conf"
+alias edzconf="nvim ~/.zshrc"
 
 alias vim="nvim"
 alias k="kubectl"
+alias c="clear"
+
+export PATH=$HOME/flutter/bin:$PATH
+export PATH="/opt/homebrew/opt/ruby@2.7/bin:$PATH"
 
 export GPG_TTY=$(tty)
