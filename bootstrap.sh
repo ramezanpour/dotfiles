2a48581aedc5b6fd0d826601097cf29a230f4832
Mohammad M. Ramezanpour <i@ramezanpour.net>
2025-12-25 22:17:24 +0100

    Remove some uncessary packages as I'm installing them via Mason

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,15 +5,9 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Some nvim plugins require yarn to be installed
 npm install -g yarn
-npm install -g prettier
-npm install -g pyright
-npm install -g eslint
 
 mkdir -p ~/.config/nvim
 cp -n ./nvim/init.lua ~/.config/nvim/
-
-mkdir -p ~/.config/bat
-cp -n ./bat/config ~/.config/bat/config
 
 mkdir -p ~/.config/tmux/
 cp -n ./tmux/tmux.conf ~/.config/tmux/tmux.conf
