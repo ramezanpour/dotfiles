87969f913e3ca011d0fac51fe6c12d32fee37897
Mohammad M. Ramezanpour <mohammadmahdi.ramezanpour@deliveryhero.com>
2026-01-09 16:06:12 +0100

    Add gitignore to ignore lazy-lock from now on. Add keymaps to prevent clipboard manipulation when using `c`

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,6 +8,8 @@ vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files"
 vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { noremap = true })
+vim.keymap.set({ "n", "v" }, "C", '"_C', { noremap = true })
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 vim.keymap.set("n", "gv", function()
