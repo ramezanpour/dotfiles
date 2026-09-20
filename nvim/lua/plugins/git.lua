2a53f8e7a439fb8b9ced26be3670cfe574b3b7a9
Mohammad M. Ramezanpour <i@ramezanpour.net>
2026-09-20 16:36:22 +0200

    Add gitsigns plugin to indicate the git changes

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,81 @@
 return {
 	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "▁" },
+				topdelete = { text = "▔" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+			signcolumn = true,
+			numhl = false,
+			word_diff = false,
+			diff_opts = {
+				internal = true,
+				algorithm = "histogram",
+				linematch = 60, -- second-stage diff: aligns changed lines within a hunk
+				indent_heuristic = true,
+			},
+			current_line_blame = false, -- toggle with <leader>gB
+			current_line_blame_opts = {
+				virt_text_pos = "eol",
+				delay = 300,
+			},
+			preview_config = { border = "rounded" },
+			on_attach = function(bufnr)
+				local gs = require("gitsigns")
+				local function map(mode, lhs, rhs, desc)
+					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+				end
+
+				-- Navigation between hunks
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gs.nav_hunk("next")
+					end
+				end, "Next git hunk")
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gs.nav_hunk("prev")
+					end
+				end, "Previous git hunk")
+
+				-- Actions
+				map("n", "<leader>gp", gs.preview_hunk_inline, "Preview hunk (inline)")
+				map("n", "<leader>gP", gs.preview_hunk, "Preview hunk (float)")
+				-- stage_hunk toggles: run it again on a staged hunk to unstage
+				map("n", "<leader>gs", gs.stage_hunk, "Stage/unstage hunk")
+				map("v", "<leader>gs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Stage/unstage selected lines")
+				map("n", "<leader>gR", gs.reset_hunk, "Reset hunk")
+				map("v", "<leader>gR", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Reset selected lines")
+				map("n", "<leader>gS", gs.stage_buffer, "Stage buffer")
+				map("n", "<leader>gb", gs.blame_line, "Blame line")
+				map("n", "<leader>gB", gs.toggle_current_line_blame, "Toggle inline blame")
+				map("n", "<leader>gd", gs.diffthis, "Diff against index")
+				map("n", "<leader>gD", function()
+					gs.diffthis("~")
+				end, "Diff against last commit")
+				map("n", "<leader>gq", gs.setqflist, "Hunks to quickfix")
+
+				-- Text object: ih = "in hunk"
+				map({ "o", "x" }, "ih", gs.select_hunk, "Select hunk")
+			end,
+		},
+	},
+	{
 		"rhysd/git-messenger.vim",
 		keys = {
 			{ "<leader>gm", "<Plug>(git-messenger)", desc = "Git Messenger (blame popup)" },
